ce1a025bfa15270d7ba26be0ae557dbe8416aa85
CZY774 <118541672+CZY774@users.noreply.github.com>
2024-10-04 09:41:16 +0700

    Update pertemuan4_dfs.py

--- pertemuan4_dfs.py
+++ pertemuan4_dfs.py
@@ -1,5 +1,9 @@
 # Fungsi untuk menambahkan edge ke dalam graf
 def tambah_edge(graf, awal, tujuan):
+    if awal not in graf:
+        graf[awal] = []
+    if tujuan not in graf:
+        graf[tujuan] = []
     graf[awal].append(tujuan)
     graf[tujuan].append(awal)  # Karena graf tidak berarah
 
@@ -8,16 +12,15 @@ def dfs_rekursif(graf, node, dikunjungi):
     # Tandai node saat ini sebagai dikunjungi
     dikunjungi[node] = True
     print(node, end=' ')
-
     # Kunjungi semua tetangga yang belum dikunjungi
     for tetangga in graf[node]:
-        if not dikunjungi[tetangga]:
+        if tetangga not in dikunjungi or not dikunjungi[tetangga]:
             dfs_rekursif(graf, tetangga, dikunjungi)
 
 # Fungsi utama untuk memulai DFS
 def dfs(graf, mulai):
     # Inisialisasi semua node sebagai belum dikunjungi
-    dikunjungi = [False] * len(graf)
+    dikunjungi = {node: False for node in graf}
     
     # Panggil fungsi rekursif DFS
     dfs_rekursif(graf, mulai, dikunjungi)
@@ -26,12 +29,15 @@ def dfs(graf, mulai):
 if __name__ == "__main__":
     # Buat graf dalam bentuk dictionary
     graf = {
-        0: [1, 2],
-        1: [0, 3, 4],
-        2: [0, 5],
-        3: [1],
-        4: [1, 5],
-        5: [2, 4]
+        0: [1, 3, 2],
+        1: [0, 3],
+        3: [0, 1, 4, 5, 2],
+        2: [0, 3, 5, 6],
+        4: [3, 8, 7, 5],
+        5: [3, 2, 4, 6, 7],
+        6: [2, 5, 7, 8],
+        7: [4, 5, 6, 8],
+        8: [4, 6, 7]
     }
 
     print("Hasil DFS dimulai dari node 0:")
